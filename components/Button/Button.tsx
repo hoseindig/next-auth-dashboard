4c2f5af8aa28fda389792bc1f0732b24a9728ff6
hoseinx <hoseinx@gmail.com>
2025-08-04 11:23:44 +0330

    add loading

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -5,11 +5,17 @@ import { ReactNode } from "react";
 interface ButtonProps {
   children: ReactNode;
   onClick: () => void;
+  loading: boolean;
 }
 
-export default function Button({ children, onClick }: ButtonProps) {
+export default function Button({ children, onClick, loading }: ButtonProps) {
   return (
-    <MuiButton className={styles.button} variant="contained" onClick={onClick}>
+    <MuiButton
+      className={styles.button}
+      variant="contained"
+      onClick={onClick}
+      loading={loading}
+    >
       {children}
     </MuiButton>
   );
